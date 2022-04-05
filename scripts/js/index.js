1658d91e61dd231c727d668c1474b9f0dd67489a
iasjem <jemimah.beryl.sai@gmail.com>
2022-04-05 13:35:41 +0800

    wip: Create UI for homepage
    - Add navigation sidebar (just look)
    - Add more mixins

--- scripts/js/index.js
+++ scripts/js/index.js
@@ -1,39 +1,92 @@
 "use strict";
 var pageLogin = document.querySelector('.login-page');
 var pageHome = document.querySelector('.home-page');
+var navigationSidebarContainer = document.querySelector('.navigation-side-bar-container');
 var buttonGoToLoginForm = document.getElementById('go_to_login_form');
 var buttonGoToRegiterForm = document.getElementById('go_to_register_form');
 var buttonUserLogin = document.getElementById('login_button');
 var buttonUserRegister = document.getElementById('register_button');
+var buttonNavigationMenu = document.getElementById('navigation_menu_button');
+var buttonCloseNavSidebar = document.getElementById('close_nav_side_bar_button');
+var buttonUserLogout = document.getElementById('logout_button');
 var formLogin = document.querySelector('.login-form');
 var formRegister = document.querySelector('.register-form');
 var showLoginForm = function () {
     formLogin.style.display = "block";
-    formRegister.style.display = "none";
+};
+var hideLoginForm = function () {
+    formLogin.style.display = "none";
 };
 var showRegisterForm = function () {
-    formLogin.style.display = "none";
     formRegister.style.display = "block";
 };
-var goToLoginPage = function () {
-    pageLogin.style.display = "block";
-    pageHome.style.display = "none";
+var hideRegisterForm = function () {
+    formRegister.style.display = "none";
 };
-var goToHomePage = function () {
+var showNavSidebar = function () {
+    navigationSidebarContainer.style.display = "block";
+};
+var hideNavSidebar = function () {
+    navigationSidebarContainer.style.display = "none";
+};
+var showLoginPage = function () {
+    pageLogin.style.display = "block";
+};
+var hideLoginPage = function () {
     pageLogin.style.display = "none";
+};
+var showHomepage = function () {
     pageHome.style.display = "block";
 };
-buttonUserLogin.addEventListener('click', function () {
+var hideHomepage = function () {
+    pageHome.style.display = "none";
+};
+var displayLoginForm = function () {
+    showLoginForm();
+    hideRegisterForm();
+};
+var displayRegisterForm = function () {
+    showRegisterForm();
+    hideLoginForm();
+};
+var goToLoginPage = function () {
+    showLoginPage();
+    hideHomepage();
+};
+var goToHomePage = function () {
+    hideLoginPage();
+    showHomepage();
+};
+var loginUser = function () {
     goToHomePage();
+};
+var logoutUser = function () {
+    hideNavSidebar();
+    goToLoginPage();
+};
+buttonUserLogin.addEventListener('click', function () {
+    loginUser();
 });
 buttonUserRegister.addEventListener('click', function () {
-    goToHomePage();
+    loginUser();
 });
 buttonGoToLoginForm.addEventListener('click', function (e) {
     e.preventDefault();
-    showLoginForm();
+    displayLoginForm();
 });
 buttonGoToRegiterForm.addEventListener('click', function (e) {
     e.preventDefault();
-    showRegisterForm();
+    displayRegisterForm();
+});
+buttonNavigationMenu.addEventListener('click', function (e) {
+    e.preventDefault();
+    showNavSidebar();
+});
+buttonCloseNavSidebar.addEventListener('click', function (e) {
+    e.preventDefault();
+    hideNavSidebar();
+});
+buttonUserLogout.addEventListener('click', function (e) {
+    e.preventDefault();
+    logoutUser();
 });
--- scripts/ts/index.ts
+++ scripts/ts/index.ts
@@ -1,11 +1,15 @@
 /** DOM Elements */
 const pageLogin = document.querySelector('.login-page') as HTMLDivElement;
 const pageHome = document.querySelector('.home-page') as HTMLDivElement;
+const navigationSidebarContainer = document.querySelector('.navigation-side-bar-container') as HTMLDivElement;
 
 const buttonGoToLoginForm = document.getElementById('go_to_login_form') as HTMLButtonElement;
 const buttonGoToRegiterForm = document.getElementById('go_to_register_form') as HTMLButtonElement;
 const buttonUserLogin = document.getElementById('login_button') as HTMLButtonElement;
 const buttonUserRegister = document.getElementById('register_button') as HTMLButtonElement;
+const buttonNavigationMenu = document.getElementById('navigation_menu_button') as HTMLButtonElement;
+const buttonCloseNavSidebar = document.getElementById('close_nav_side_bar_button') as HTMLButtonElement;
+const buttonUserLogout = document.getElementById('logout_button') as HTMLButtonElement;
 
 const formLogin = document.querySelector('.login-form') as HTMLDivElement;
 const formRegister = document.querySelector('.register-form') as HTMLDivElement;
@@ -14,40 +18,104 @@ const formRegister = document.querySelector('.register-form') as HTMLDivElement;
 /** Functions */
 const showLoginForm = () => {
     formLogin.style.display = "block";
-    formRegister.style.display = "none";
+}
+
+const hideLoginForm = () => {
+    formLogin.style.display = "none";
 }
 
 const showRegisterForm = () => {
-    formLogin.style.display = "none";
     formRegister.style.display = "block";
 }
 
-const goToLoginPage = () => {
+const hideRegisterForm = () => {
+    formRegister.style.display = "none";
+}
+
+const showNavSidebar = () => {
+    navigationSidebarContainer.style.display = "block";
+}
+
+const hideNavSidebar = () => {
+    navigationSidebarContainer.style.display = "none";
+}
+
+const showLoginPage = () => {
     pageLogin.style.display = "block";
+}
+
+const hideLoginPage = () => {
+    pageLogin.style.display = "none";
+}
+
+const showHomepage = () => {
+    pageHome.style.display = "block";
+}
+
+const hideHomepage = () => {
     pageHome.style.display = "none";
 }
 
+const displayLoginForm = () => {
+    showLoginForm();
+    hideRegisterForm();
+}
+
+const displayRegisterForm = () => {
+    showRegisterForm();
+    hideLoginForm();
+}
+
+const goToLoginPage = () => {
+    showLoginPage();
+    hideHomepage();
+}
+
 const goToHomePage = () => {
-    pageLogin.style.display = "none";
-    pageHome.style.display = "block";
+    hideLoginPage();
+    showHomepage();
+}
+
+const loginUser = () => {
+    goToHomePage();
+}
+
+const logoutUser = () => {
+    hideNavSidebar();
+    goToLoginPage();
 }
 
 
 /** DOM Events */
 buttonUserLogin.addEventListener('click', () => {
-    goToHomePage();
+    loginUser();
 });
 
 buttonUserRegister.addEventListener('click', () => {
-    goToHomePage();
+    loginUser();
 });
 
 buttonGoToLoginForm.addEventListener('click', (e) => {
     e.preventDefault();
-    showLoginForm();
+    displayLoginForm();
 });
 
 buttonGoToRegiterForm.addEventListener('click', (e) => {
     e.preventDefault();
-    showRegisterForm();
+    displayRegisterForm();
 });
+
+buttonNavigationMenu.addEventListener('click', (e) => {
+    e.preventDefault();
+    showNavSidebar();
+});
+
+buttonCloseNavSidebar.addEventListener('click', (e) => {
+    e.preventDefault();
+    hideNavSidebar();
+});
+
+buttonUserLogout.addEventListener('click', (e) => {
+    e.preventDefault();
+    logoutUser();
+})
