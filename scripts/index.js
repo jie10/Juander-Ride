a1e70d544af63294b923cc2fcf13068b8bf310e1
Jemimah Sai <jemimah.beryl.sai@gmail.com>
2022-05-26 22:44:18 +0800

    Add validation for login form

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,30 @@
-function login() {
-    var email = document.getElementById("email");
+var user_email = document.getElementById('user_email');
+var login_button = document.getElementById('login_button');
+var login_form = document.querySelector('.login-form');
 
-    if (email == "techinnov@cebupacificair.com") {
-        window.location.href = "newhomepage.html";
+var USER_LOGIN_EMAIL_KEY = 'email';
+var hasError = false;
+
+function highlightErrorInput() {
+    user_email.style.borderColor = hasError ? 'red' : '#ced4da';
+    document.querySelector('.invalid-feedback').style.display = hasError ? 'block' : 'none';
+}
+function onTyping() {
+    hasError = false;
+    highlightErrorInput();
+}
+function onLogin() {
+    var emailPattern = /(@cebupacificair.com)/gi;
+    var email = user_email.value;
+
+    if (emailPattern.test(email) === true && email.length > 0) {
+        localStorage.setItem(USER_LOGIN_EMAIL_KEY, email);
+        console.log(localStorage.getItem(USER_LOGIN_EMAIL_KEY));
     } else {
-        var errMsg = document.getElementById('errMsg');
-        errMsg.style.display = "block";
-        errMsg.style.color = "red";
+        hasError = true;
+        highlightErrorInput();
     }
 }
 
-function loginAsGuest() {
-    window.location.href = "guestpage.html";
-}
+user_email.addEventListener('keyup', onTyping);
+login_button.addEventListener('click', onLogin);
